e78a355e4f3eb7c7f98a3bee67fdeb207d38991b
Labhansh Agrawal <labhansh.agrawal@gmail.com>
2021-06-03 13:35:46 +0530

    e

--- api/index.ts
+++ api/index.ts
@@ -1,17 +1,15 @@
 import { readFileSync } from 'fs'
 import { join as joinPath } from 'path'
-import { NowRequest, NowResponse } from '@now/node'
+import { VercelRequest, VercelResponse } from '@vercel/node'
 import { satisfies } from 'semver'
-import { Url } from 'url'
-
 
 // Set Type for Messages
 interface Messages {
-  text: String,
-  url?: Url,
-  dismissable: Boolean,
-  versions: Array<String>,
-  platforms: Array<String>
+  text: string;
+  url?: string;
+  dismissable: boolean;
+  versions: string[];
+  platforms: string[];
 }[]
 
 // Get news JSON
@@ -19,21 +17,21 @@ const news = JSON.parse(readFileSync(joinPath(__dirname, '../news.json'), 'utf-8
 const legacyNews = JSON.parse(readFileSync(joinPath(__dirname, '../legacy-news.json'), 'utf-8'))
 
 // Match versions (strings) from two sources
-const matchVersion = (versions: Array<String>, clientVersion: string | string[]) => (
+const matchVersion = (versions: string[], clientVersion: string) => (
   versions.some(v => v === '*' || satisfies(clientVersion, v))
 )
 
 // Match platform (strings) from two sources
-const matchPlatform = (platforms: Array<String>, clientPlatform: string | string[]) => (
+const matchPlatform = (platforms: string[], clientPlatform: string) => (
   platforms.some(p => p === '*' || p === clientPlatform)
 )
 
 // Main function export
-export default (req: NowRequest, res: NowResponse) => {
+export default (req: VercelRequest, res: VercelResponse) => {
   // Get platform and version headers, which should be sent from Hyper.app
-  const platform = req.headers['x-hyper-platform']
-  const version = req.headers['x-hyper-version']
-  
+  const platform = req.headers['x-hyper-platform'] as string
+  const version = req.headers['x-hyper-version'] as string
+
   console.log(JSON.stringify({platform, version}))
 
   // If platform and version aren't defined, assume legacy Hyper version and respond with legacy message
